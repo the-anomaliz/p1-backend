e8d5054f3d8fdc573bfbb075125b206f52fd4879
Garima Sharma <sharmagarima814@gmail.com>
2023-06-18 19:16:37 +0530

    add: auth

---
+++ config/passport.js
@@ -0,0 +1,39 @@
+import { Strategy as LocalStrategy } from 'passport-local';
+import bcrypt from 'bcryptjs';
+
+//User model
+import User from '../models/user.model.js';
+
+// Passport function
+export default function (passport) {
+  passport.use(
+    'local',
+    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+      User.findOne({ email: email })
+        .then(user => {
+          if (!user) {
+            return done(null, false, { message: 'Email not registered' });
+          }
+          bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) throw err;
+            if (isMatch) {
+              return done(null, user);
+            } else {
+              return done(null, false, { message: 'Password incorrect' });
+            }
+          });
+        })
+        .catch(err => console.log(err));
+    })
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id).then((err, user) => {
+      done(err, user);
+    });
+  });
+}
---
+++ models/user.model.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+  {
+    full_name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ['company', 'student'],
+      default: 'student',
+    },
+    secretToken: {
+      type: String,
+      required: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model('User', UserSchema);
